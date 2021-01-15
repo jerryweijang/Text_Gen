04b409b9dcaf1eae8f2297229c4b1563e031dcb6
Jerry Chen <jerryweijang@hotmail.com>
2021-01-15 15:10:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,16 @@ st.title("TEXTGEN App")
 ai = aitextgen()
 
 # create a prompt text for the text generation 
-#prompt_text = "Python is awesome"
-prompt_text = st.text_input(label = "Enter your prompt text...",
-            value = "The point is")
+col1,col2 = st.beta_columns(2)
+with col1:
+    prompt_text = st.text_input(label = "Enter your prompt text...", value = "The point is")
+with col2:
+    m_length = st.number_input("Length", min_value=1, max_value=quantity, step=1)               
 
 with st.spinner("AI is Working~"):
     # text generation
     gpt_text = ai.generate_one(prompt=prompt_text,
-            max_length = 100 )
+            max_length = m_length )
 st.success("AI Successfully generated the below text ")
 st.balloons()
 # print ai generated text
