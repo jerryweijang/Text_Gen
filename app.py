79ef2cab8c1b4af36bdabdb3a2a982d2aafd3cd9
Jerry Chen <jerryweijang@hotmail.com>
2021-01-22 14:03:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,22 +8,24 @@ st.title("TEXT GEN")
 # instantiate the model / download
 ai = aitextgen()
 
+# Initial
+translator = googletrans.Translator()
+
 # create a prompt text for the text generation 
 col1,col2 = st.beta_columns(2)
 with col1:
-    prompt_text = st.text_input(label = "Enter your prompt text...", value = "The point is")
+    prompt_text = st.text_input(label = "Enter your Chinese prompt text...", value = "哈利波特")
 with col2:
     m_length = st.number_input("Length", min_value=1, step=1)        
 
 if st.button('Generate'):
     with st.spinner("AI is Working~"):
-        # text generation
-        gpt_text = ai.generate_one(prompt=prompt_text,
+        e_results = translator.translate(prompt_text, dest='us').text
+        gpt_text = ai.generate_one(prompt=e_results,
             max_length = m_length )
 #st.success("AI Successfully generated the below text ")
     st.success(gpt_text)
-    # Initial
-    translator = googletrans.Translator()
+   
 
     # Basic Translate
     #results = translator.translate('我覺得今天天氣不好。')
